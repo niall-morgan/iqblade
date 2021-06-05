30acda8b9c0ec01dcfdcd40ecc5e222a73a2cc31
Niall Morgan <niall.morgan@iqblade.com>
2021-06-05 12:19:14 +0100

    adding needed files for basic project

--- src/main/webapp/app/shared/tables/pagination.tsx
+++ src/main/webapp/app/shared/tables/pagination.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
 export interface IJhiPaginationProps {
-  items: number;
   activePage: number;
   onSelect: (page: number) => void;
   maxButtons: number;
+  totalItems: number;
+  itemsPerPage: number;
 }
 
 export interface IJhiPaginationState {
@@ -39,14 +40,15 @@ export class JhiPagination extends React.Component<IJhiPaginationProps, IJhiPagi
     const items = [];
     let item: any = {};
     let previousItem: any = {};
+    const maxPage = this.getMaxPage();
     const padSup = Math.floor((this.props.maxButtons - 1) / 2);
     const modulo = (this.props.maxButtons - 1) % 2;
     const padInf = padSup + modulo;
-    for (let j = 0; j < this.props.items; j++) {
+    for (let j = 0; j < maxPage; j++) {
       item = {};
       if (
         j === 0 ||
-        j === this.props.items - 1 ||
+        j === maxPage - 1 ||
         j === activePage - 1 ||
         j === activePage - 2 ||
         (activePage === 1 && j === 1) ||
@@ -67,43 +69,56 @@ export class JhiPagination extends React.Component<IJhiPaginationProps, IJhiPagi
     return items;
   };
 
-  displayPaginationItem = i => (
-    <PaginationItem key={i}>
-      <PaginationLink onClick={this.updateActivePage(i + 1)} href="javascript:void(0)">
-        {i + 1}
-      </PaginationLink>
+  displayPaginationItem = (i, activePage) => (
+    <PaginationItem {...(activePage === i + 1 && { active: true })} key={i}>
+      <PaginationLink onClick={this.updateActivePage(i + 1)}>{i + 1}</PaginationLink>
     </PaginationItem>
   );
 
+  cleanActivePage = () => {
+    const { totalItems, itemsPerPage, activePage } = this.props;
+    const cleanActivePage = totalItems === 0 ? 1 : Math.min(activePage, Math.ceil(totalItems / itemsPerPage));
+
+    if (cleanActivePage !== activePage) {
+      this.updateActivePage(cleanActivePage)();
+    }
+  };
+
+  getMaxPage = () => {
+    const { itemsPerPage, totalItems } = this.props;
+    const division = Math.floor(totalItems / itemsPerPage);
+    const modulo = totalItems % itemsPerPage;
+    return division + (modulo !== 0 ? 1 : 0);
+  };
+
   render() {
-    const { activePage, items } = this.props;
+    this.cleanActivePage();
+    const { activePage } = this.props;
+    const maxPage = this.getMaxPage();
+
     return (
       <div>
         <Pagination>
           <PaginationItem {...(activePage === 1 && { disabled: true })}>
-            <PaginationLink onClick={this.updateActivePage(1)} href="javascript:void(0)">
-              ««
-            </PaginationLink>
+            <PaginationLink first onClick={this.updateActivePage(1)} />
           </PaginationItem>
           <PaginationItem {...(activePage === 1 && { disabled: true })}>
-            <PaginationLink previous onClick={this.previousPage} href="javascript:void(0)" />
+            <PaginationLink previous onClick={this.previousPage} />
           </PaginationItem>
           {this.itemsToDisplay(activePage).map((paginationItem, i) =>
             paginationItem.display === 'display' ? (
-              this.displayPaginationItem(i)
+              this.displayPaginationItem(i, activePage)
             ) : paginationItem.display === 'disabled' ? (
               <PaginationItem disabled key={i}>
-                <PaginationLink href="#">...</PaginationLink>
+                <PaginationLink>...</PaginationLink>
               </PaginationItem>
             ) : null
           )}
-          <PaginationItem {...(activePage === items && { disabled: true })}>
-            <PaginationLink next onClick={this.nextPage} href="javascript:void(0)" />
+          <PaginationItem {...(activePage === maxPage && { disabled: true })}>
+            <PaginationLink next onClick={this.nextPage} />
           </PaginationItem>
-          <PaginationItem {...(activePage === items && { disabled: true })}>
-            <PaginationLink onClick={this.updateActivePage(items)} href="javascript:void(0)">
-              »»
-            </PaginationLink>
+          <PaginationItem {...(activePage === maxPage && { disabled: true })}>
+            <PaginationLink last onClick={this.updateActivePage(maxPage)} />
           </PaginationItem>
         </Pagination>
       </div>
